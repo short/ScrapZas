c4e8c738f1f415dd79a96d0ee84072c7cc9d1629
rickkorteman <rickkorteman@hotmail.com>
2019-10-08 12:34:34 +0200

    keyword and url list added

---
+++ keywordlist.py
@@ -0,0 +1,16 @@
+class keywordlist:
+
+    keywordlist = list()
+
+    def __init__(self):
+        self.keywordlist = [
+            "utrecht", "softdrugs", "harddrugs", "hasj", "xtc", "mdma", "pma", "speed", "pep", "heroine", "sos", "M",
+            "dexies", "truffels", "paddo's", "dexamfetamine", "creak", "wiet", "GHB", "cocaine", "delivery", "versneden",
+            "pasta", "gram", "milligram", "mg", "delivery", "bezorgen", "dealers", "ecstasy", "snuif", "snuiven",
+            "snatsen", "kwaliteit", "dealer", "pillen", "festivals", "festival", "lachgas", "amfetamine", "cannabis",
+            "assie", "coke", "wit", "liter", "kilo", "k", "shop", "naalden", "smartshop", "smart shop", "magic",
+            "mushrooms", "locatie", "sell", "buy", "kopen", "verkopen"
+        ]
+
+    def get_keywordlist(self):
+        return self.keywordlist
---
+++ urllist.py
@@ -0,0 +1,16 @@
+class urllist:
+
+    urllist = list()
+
+    def __init__(self):
+        self.urllist = [
+            "https://drugs-forum.com/",
+            "https://forum.drugs.com/",
+            "https://www.drugsforum.nl/",
+            "https://www.reddit.com/r/Drugs/",
+            "https://twitter.com/search",
+            ""
+        ]
+
+    def get_urllist(self):
+        return self.urllist
