6712341141c054f88a3d0fba10ff64f3db5b4277
rickkorteman <rickkorteman@hotmail.com>
2019-10-10 10:49:17 +0200

    deeplink

--- deeplinkscraper.py
+++ deeplinkscraper.py
@@ -35,23 +35,21 @@ class Worker:
     def work(self):
         for link in self.queue:
 
-            try:
-                page = urlopen(link)
-                soup = BeautifulSoup(page, 'lxml')
+            if ".jpg" or ".JPG" or ".JPEG" or ".png" or ".PNG" not in link:
+                try:
+                    page = urlopen(link)
+                    soup = BeautifulSoup(page, 'lxml')
 
-                self.write_file("dump.txt", soup.text)
-                self.write_file("log.txt", link + "\n")
-                self.report(link)
-                self.crawled.add(link)
+                    self.write_file("dump.txt", soup.text)
+                    self.write_file("log.txt", link + "\n")
+                    self.report(link)
+                    self.crawled.add(link)
 
-                for upper_domain in soup.find_all('a', href=True):
-                    joined_link = urljoin(self.base_url, upper_domain['href'])
-                    if joined_link not in self.crawled:
-                        self.queue.append(joined_link)
-
-            except:
-                # log any failed URL crawls and continue
-                self.write_file("error_log.txt", str(link) + "\n")
-                pass
-
-
+                    for upper_domain in soup.find_all('a', href=True):
+                        joined_link = urljoin(self.base_url, upper_domain['href'])
+                        if joined_link not in self.crawled:
+                            self.queue.append(joined_link)
+                except:
+                    # log any failed URL crawls and continue
+                    self.write_file("error_log.txt", str(link) + "\n")
+                    pass
