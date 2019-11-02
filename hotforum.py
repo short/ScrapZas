ecd9bfd9df07749d2eba85eadf00d9f4945687bb
rickkorteman <rickkorteman@hotmail.com>
2019-11-02 14:44:42 +0100

    hotforum session fix

--- hotforum.py
+++ hotforum.py
@@ -1,15 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import telegram
-import time
-import re
-import json
 
 forumlink = "https://www.hotforum.nl/forum-overzicht/Business+en+Finance"
 #geef hier je max bedrag op
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
-#aanvraag = {'accept=true']
 drugsgebruikers = []
 
 
@@ -18,19 +13,19 @@ def getuserlink():
         a = requests.get(forumlink, headers=headers)
         b = a.content
         soup = BeautifulSoup(b, "html.parser")
-        #print(soup)
 
         for li in soup.find_all(class_="Visit"):
             print(li.a.get('href'))
 
-def getpageinfo(a):
-    r = requests.post("https://www.hotforum.nl/forum/index.php?name=Scotty&amp;go=onderwerp&amp;id=910908&amp;subject=xtc-speed-ghb-wiet-247-leverbaar/&amp")
-    scrape = requests.get(a, headers=headers)
+
+def getpageinfo(a, session):
+    scrape = session.get(a, headers=headers)
     websitecontent = scrape.content
     soup = BeautifulSoup(websitecontent, "html.parser")
 
     for li in soup.find_all(class_="onderwerp"):
         print(li)
+
 
 def test():
     url = 'https://www.hotforum.nl/forum/index.php?name=Drugsinc&'
@@ -45,25 +40,17 @@ def test():
         "Referer": "https://www.hotforum.nl/forum/Drugsinc",
         "Accept-Encoding": "gzip,deflate,br",
         "Accept-Language": "nl,en-US;q=0.7,en;q=0.3",
-        "Cookie": "PHPSESSID=9he8o8mb1p32r302r9j1n5t8q7",
+        # "Cookie": "PHPSESSID=9he8o8mb1p32r302r9j1n5t8q7",
         "Upgrade-Insecure-Requests": 1,
         "accept": "true"
     }
 
-
-    p = {"accept": "true"
-    }
-
-
     # Adding empty header as parameters are being sent in payload
-    test = requests.post('https://www.hotforum.nl/forum/index.php?name=Drugsinc&', data=payload)
+    session = requests.Session()
+    test = session.post('https://www.hotforum.nl/forum/index.php?name=Drugsinc&', data=payload)
     print(test)
     print('werkt')
-    getpageinfo('https://www.hotforum.nl/forum/Drugsinc')
-
+    getpageinfo('https://www.hotforum.nl/forum/index.php?name=Drugsinc&', session)
 
 
 test()
-
-#getpageinfo('https://www.hotforum.nl/forum/Drugsinc')
-#getuserlink()
